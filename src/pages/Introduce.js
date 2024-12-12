8ec27a7bbd8a54014820d22efdee69dc1eb3a847
Jiwon <jiw2703@naver.com>
2024-12-13 05:56:15 +0900

    feature #6 자기소개페이지 초기 세팅

--- src/pages/Introduce.js
+++ src/pages/Introduce.js
@@ -4,20 +4,25 @@ import { ReactMediaRecorder } from "react-media-recorder";
 function Introduce() {
   return (
     <div>
-      <h1>
+      <div className='wrapper'>
+        <h2 className='intro'>
+          카메라를 키고 1분간 자기소개를 녹화해주세요
+        </h2>
+        <div className='video'>
+          <ReactMediaRecorder c
+          video
+          render={({ status, startRecording, stopRecording, mediaBlobUrl }) =>(
+            <div>
+              <button className='startBtn' onClick={startRecording}>start recording</button> 
+              <button className='stopBtn' onClick={stopRecording}>stop recording</button><br/><br/>
+              <p>{status}</p>
+              <video src={mediaBlobUrl} controls></video><br/>
+            </div>  
+          )}
+        />
+        </div>
         
-      </h1>
-      <ReactMediaRecorder
-        video
-        render={({ status, startRecording, stopRecording, mediaBlobUrl }) =>(
-          <div>
-            <p>{status}</p>
-            <button onClick={startRecording}>start recording</button>
-            <button onClick={stopRecording}>stop recording</button><br/><br/>
-            <video src={mediaBlobUrl} controls></video><br/>
-          </div>  
-        )}
-      />
+      </div>
     </div>
   );
 }
