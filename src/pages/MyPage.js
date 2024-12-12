9c080dcaca9a679b7f50b436f6d6ec189e3dfe7e
Jiwon <jiw2703@naver.com>
2024-12-13 06:00:36 +0900

    feature #5 마이페이지 레이아웃

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -3,9 +3,11 @@ import './MyPage.css'
 function MyPage() {
   return (
     <div>
-      <h1>
-        This is MyPage
-      </h1>
+      <div className='wrapper'>
+        <h1 className='text'>
+          This is MyPage
+        </h1>
+      </div>
     </div>
   );
 }
