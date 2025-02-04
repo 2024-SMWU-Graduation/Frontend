3792e3a21da9fcef707c20abee45e9ce72620705
Jiwon <jiw2703@naver.com>
2025-02-04 14:47:42 +0900

    feat #7 분석요청 클릭 후 로딩 팝업 띄우기 수정

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import {Background, LoadingText} from './LoadingStyle';
+// import {Background, LoadingText} from './LoadingStyle';
 import Spinner from '../assets/images/Spinner.gif';
 
-export default () => {
-    return (
-      <Background>
-        <LoadingText>잠시만 기다려 주세요.</LoadingText>
-        <img src={Spinner} alt="로딩중" width="5%" />
-      </Background>
-    );
-  };
+const Loading = () => {
+  return (
+    <div>
+      <h3>잠시만 기다려주세요..</h3>
+      <img src={Spinner} alt='로딩' width="15%" />
+    </div>
+  );
+};
+
+export default Loading;
+
+
+// export default () => {
+//     return (
+//       <Background>
+//         <LoadingText>잠시만 기다려 주세요.</LoadingText>
+//         <img src={Spinner} alt="로딩중" width="5%" />
+//       </Background>
+//     );
+// };
